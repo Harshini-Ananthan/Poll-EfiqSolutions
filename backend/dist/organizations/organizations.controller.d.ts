3ae6ab9352916ed7e2fd882c4d3e030f3907daea
Madhusree M <mmadhusreemayilsamy@gmail.com>
2026-05-14 21:33:40 +0530

    User Management Done

--- backend/dist/organizations/organizations.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { OrganizationsService } from './organizations.service';
-export declare class OrganizationsController {
-    private readonly organizationsService;
-    constructor(organizationsService: OrganizationsService);
-    getProfile(req: any): Promise<any>;
-    updateProfile(req: any, data: any): Promise<any>;
-}
